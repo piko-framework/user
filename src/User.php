05f2ca62156a783f25f80328be06bd3033e6a207
ilhooq <contact@sphilip.com>
2021-09-25 12:37:49 +0200

    Pre-release

---
+++ src/User.php
@@ -0,0 +1,178 @@
+<?php
+
+/**
+ * This file is part of Piko - Web micro framework
+ *
+ * @copyright 2019-2021 Sylvain PHILIP
+ * @license LGPL-3.0; see LICENSE.txt
+ * @link https://github.com/piko-framework/piko
+ */
+
+declare(strict_types=1);
+
+namespace piko;
+
+use RuntimeException;
+
+/**
+ * Application User base class.
+ *
+ * @method boolean checkAccess(string|int|null $id, string $permission) A user defined behavior method
+ * to check user permission.
+ *
+ * @author Sylvain PHILIP <contact@sphilip.com>
+ */
+class User extends Component
+{
+    /**
+     * The class name of the identity object.
+     *
+     * @var string
+     */
+    public $identityClass;
+
+    /**
+     * The number of seconds in which the user will be logged out automatically if he remains inactive.
+     *
+     * @var integer
+     */
+    public $authTimeout;
+
+    /**
+     * The identity instance.
+     *
+     * @var IdentityInterface|null
+     */
+    protected $identity;
+
+    /**
+     * Internal cache of access permissions.
+     *
+     * @var array<boolean>
+     */
+    protected $access = [];
+
+    /**
+     * {@inheritDoc}
+     * @see \piko\Component::init()
+     */
+    protected function init(): void
+    {
+        $this->trigger('init', [$this]);
+
+        if (!empty($this->authTimeout && session_status() !== PHP_SESSION_ACTIVE)) {
+            ini_set('session.gc_maxlifetime', (string) $this->authTimeout);
+        }
+    }
+
+    protected function startSession(): void
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+
+    /**
+     * Get user identity
+     *
+     * @return IdentityInterface|null The user identity or null if no identity is found.
+     */
+    public function getIdentity(): ?IdentityInterface
+    {
+        $this->startSession();
+
+        if ($this->identity === null && isset($_SESSION['_id'])) {
+            $class = $this->identityClass;
+            $this->identity = $class::findIdentity($_SESSION['_id']);
+        }
+
+        return $this->identity;
+    }
+
+    /**
+     * Get user identifier.
+     *
+     * @return string|int|null
+     */
+    public function getId()
+    {
+        $identity = $this->getIdentity();
+
+        return $identity !== null ? $identity->getId() : null;
+    }
+
+    /**
+     * Set user identity.
+     *
+     * @param IdentityInterface $identity The user identity.
+     * @return void
+     * @throws RuntimeException If identiy doesn't implement IdentityInterface.
+     */
+    public function setIdentity(IdentityInterface $identity): void
+    {
+        $this->identity = $identity;
+        $this->access = [];
+    }
+
+    /**
+     * Start the session and set user identity.
+     *
+     * @param IdentityInterface $identity The user identity.
+     * @return void
+     */
+    public function login(IdentityInterface $identity): void
+    {
+        $this->startSession();
+        $this->trigger('beforeLogin', [$identity]);
+        $this->setIdentity($identity);
+        $this->trigger('afterLogin', [$identity]);
+        $_SESSION['_id'] = $identity->getId();
+    }
+
+    /**
+     * Destroy the session and remove user identity.
+     *
+     * @return void
+     */
+    public function logout(): void
+    {
+        $this->startSession();
+        $this->trigger('beforeLogout', [$this->identity]);
+        session_destroy();
+        $this->trigger('afterLogout', [$this->identity]);
+        $this->identity = null;
+        $this->access = [];
+    }
+
+    /**
+     * Returns a value indicating whether the user is a guest (not authenticated).
+     *
+     * @return boolean whether the current user is a guest.
+     */
+    public function isGuest(): bool
+    {
+        return $this->getIdentity() === null;
+    }
+
+    /**
+     * Check if the user can do an action.
+     *
+     * @param string $permission The permission name.
+     * @return boolean
+     */
+    public function can(string $permission): bool
+    {
+        if (isset($this->access[$permission])) {
+            return $this->access[$permission];
+        }
+
+        if (!isset($this->behaviors['checkAccess'])) {
+            return false;
+        }
+
+        $access = $this->checkAccess($this->getId(), $permission);
+        $this->access[$permission] = $access;
+
+        return $access;
+    }
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,122 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use piko\User;
+use tests\mock\User as UserIdentity;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class UserTest extends TestCase
+{
+    public static function backupSession($data)
+    {
+        file_put_contents(__DIR__ . '/_session_data', serialize($data));
+    }
+
+    public static function restoreSession()
+    {
+        session_start();
+        $_SESSION = unserialize(file_get_contents(__DIR__ . '/_session_data'));
+    }
+
+    protected function getUser()
+    {
+        return new User([
+            'identityClass' => UserIdentity::class,
+            'authTimeout' => 5,
+            'behaviors' => ['checkAccess' => function($id, $permission) {
+                return $id == 1 && $permission == 'test';
+            }]
+        ]);
+    }
+
+    public function testLogin()
+    {
+        $user = $this->getUser();
+
+        $identity = UserIdentity::findIdentity(1);
+        $user->login($identity);
+        $this->assertFalse($user->isGuest());
+        $this->assertEquals(1, $user->getId());
+
+        self::backupSession($_SESSION);
+    }
+
+    /**
+     * @depends testLogin
+     */
+    public function testRetrieveIdentityFromSession()
+    {
+        self::restoreSession();
+        $user = $this->getUser();
+        $this->assertEquals('sylvain', $user->getIdentity()->username);
+    }
+
+    /**
+     * @depends testLogin
+     */
+    public function testPermissionsAfterLogin()
+    {
+        self::restoreSession();
+        $user = $this->getUser();
+        $this->assertFalse($user->isGuest());
+        $this->assertFalse($user->can('post'));
+        $this->assertTrue($user->can('test'));
+        $this->assertTrue($user->can('test')); // Cover permission cache access
+    }
+
+    /**
+     * @depends testLogin
+     */
+    public function testPermissionsWithoutAccessChecker()
+    {
+        self::restoreSession();
+        $user = $this->getUser();
+        $user->detachBehavior('checkAccess');
+        $this->assertFalse($user->can('test'));
+    }
+
+    /**
+     * @depends testLogin
+     */
+    public function testLogout()
+    {
+        self::restoreSession();
+        $user = $this->getUser();
+        $this->assertFalse($user->isGuest());
+        $user->logout();
+        $this->assertTrue($user->isGuest());
+    }
+
+
+    public function testEvents()
+    {
+        $user = new User([
+            'events' => ['init' => [function($u) {
+                $u->identityClass = UserIdentity::class;
+                ini_set('session.name', 'TEST_SESSION');
+            }]]
+        ]);
+
+        $this->assertEquals('TEST_SESSION', ini_get('session.name'));
+        $this->assertEquals(UserIdentity::class, $user->identityClass);
+
+        $user->on('afterLogin', function($identity) {
+            $identity->username = 'pascal';
+        });
+
+        $user->on('afterLogout', function($identity) {
+            $identity->username = 'sylvain';
+        });
+
+        $identity = UserIdentity::findIdentity(1);
+
+        $user->login($identity);
+
+        $this->assertEquals('pascal', $identity->username);
+
+        $user->logout();
+
+        $this->assertEquals('sylvain', $identity->username);
+    }
+}
