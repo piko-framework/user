5327037fb059905bfc0146870b725b586e73afc6
ilhooq <contact@sphilip.com>
2022-10-15 21:44:48 +0200

    Update headers

--- src/User.php
+++ src/User.php
@@ -3,9 +3,9 @@
 /**
  * This file is part of Piko - Web micro framework
  *
- * @copyright 2019-2021 Sylvain PHILIP
+ * @copyright 2019-2022 Sylvain PHILIP
  * @license LGPL-3.0; see LICENSE.txt
- * @link https://github.com/piko-framework/piko
+ * @link https://github.com/piko-framework/user
  */
 
 declare(strict_types=1);
